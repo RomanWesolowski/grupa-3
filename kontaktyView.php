20c06f070fab617e5293df4c45f8c415583eecae
Krystian Sobolewski <kleophass@gmail.com>
2016-04-19 19:01:14 +0200

    Odwołanie do skryptu logiki znajomych

--- kontaktyView.php
+++ kontaktyView.php
@@ -16,18 +16,9 @@
             <div class="table-responsive">
                 <table class="table table-hover">
                     <!-- Przykład, obok imienia będą wyświetlały się ilość wiadomości nieprzeczytanych -->
-                        <tr>
-                            <td>Andrzej</td>
-                        </tr>
-                        <tr>
-                            <td>Krystian</td>
-                        </tr>
-                        <tr>
-                            <td>Michał</td>
-                        </tr>
-                        <tr>
-                            <td>Roman</td>
-                        </tr>
+                      <?php
+                        include(kontaktyScript.php);
+                       ?>
                 </table>
             </div>
         </div>
