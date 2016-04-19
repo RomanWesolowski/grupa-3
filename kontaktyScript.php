798741b38142eeefd0c6509343a5112fcd102720
Krystian Sobolewski <kleophass@gmail.com>
2016-04-19 17:34:52 +0200

    sdvdfv

--- kontaktyScript.php
+++ kontaktyScript.php
@@ -1,24 +1,23 @@
 <?php
-  require('connect.php');
-  $baza = connect_db();
+  include("connection.php");
 
   $id_test_user = 3;
-  $wynik = @$baza->query(sprintf(
+
+  $zapytanie =
       "SELECT IMIE FROM USER
       WHERE ID_USER IN
         (SELECT za.ID_USER
         FROM ZNAJOMI za
-        WHERE za.ID_ZNAJOMY = '%d')
-      OR ID_USER IN
-	      (SELECT zb.ID_ZNAJOMY
+        WHERE za.ID_ZNAJOMY = '".$id_test_user."')".
+      "OR ID_USER IN
+  	    (SELECT zb.ID_ZNAJOMY
         FROM ZNAJOMI zb
-        WHERE zb.ID_USER = '%d')",
-        $id_test_user, $id_test_user
-    ));
+        WHERE zb.ID_USER ='".$id_test_user."');";
 
-  $ilu_userow = $wynik->num_rows;
-  while($ilu_userow>0){
-    $friend = mysqli_fetch_assoc($wynik);
-    echo "$friend";
+  $wynik = db_connection($zapytanie);
+
+  while($wiersz = mysql_fetch_array($wynik)){
+    echo $wiersz['IMIE'];
+    echo 'petla';
   }
  ?>
