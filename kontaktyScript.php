3ae13d46a8a97c385dfd9ac2244eba956cfa1d76
Krystian Sobolewski <kleophass@gmail.com>
2016-04-19 16:11:16 +0200

    Change SQL Query,
    searching for test user with id:
    id_test_user =3

--- kontaktyScript.php
+++ kontaktyScript.php
@@ -1,24 +1,24 @@
 <?php
   require('connect.php');
-  $dbxx = connect_db();
+  $baza = connect_db();
 
-  $sql = @$dbxx->query(sprintf(
+  $id_test_user = 3;
+  $wynik = @$baza->query(sprintf(
       "SELECT IMIE FROM USER
       WHERE ID_USER IN
         (SELECT za.ID_USER
         FROM ZNAJOMI za
-        WHERE za.ID_ZNAJOMY = '3')
+        WHERE za.ID_ZNAJOMY = '%s')
       OR ID_USER IN
 	      (SELECT zb.ID_ZNAJOMY
         FROM ZNAJOMI zb
-        WHERE zb.ID_USER = '3')"//,
-    //  mysqli_real_escape_string($dbxx, $_SESSION["id_user"]),
-    //  mysqli_real_escape_string($dbxx, $_SESSION["id_user"])
+        WHERE zb.ID_USER = '%s')",
+        $id_test_user, $id_test_user
     ));
 
-      $ilu_userow = $sql->num_rows;
-      while($ilu_userow>0){
-              $friend = mysqli_fetch_assoc($sql);
-              echo "$friend";
-      }
+  $ilu_userow = $wynik->num_rows;
+  while($ilu_userow>0){
+    $friend = mysqli_fetch_assoc($wynik);
+    echo "$friend";
+  }
  ?>
