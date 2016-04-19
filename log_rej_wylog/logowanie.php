b5747c719107625cfaaf617d38130f286c773746
RomanWesolowski <romanwesolowski1@wp.pl>
2016-04-19 22:10:32 +0200

    Szyfrowanie hasła

--- log_rej_wylog/logowanie.php
+++ log_rej_wylog/logowanie.php
@@ -9,8 +9,8 @@ if(!empty($_POST['l_password']) && !empty($_POST['l_email'])){
     $password = htmlentities($password, ENT_QUOTES, "UTF-8");
     
     if (empty($_SESSION) && $sql = @$dbxx->query(sprintf("SELECT * FROM USER WHERE EMAIL='%s' AND PASSWORD='%s'",
-        mysqli_real_escape_string($dbxx, $email),
-        mysqli_real_escape_string($dbxx, $password)))){
+        mysqli_real_escape_string($dbxx, $email ),
+        mysqli_real_escape_string($dbxx, md5($password) )))){
         $ilu_userow = $sql->num_rows;
         if($ilu_userow>0){
             
@@ -23,33 +23,10 @@ if(!empty($_POST['l_password']) && !empty($_POST['l_email'])){
                 $id_user = $person['ID_USER'];
                 $imie = $person['IMIE'];
                 $nazwisko = $person['NAZWISKO'];
-                $email = $person['email'];
-                $password = $person['password'];
                 
                 $_SESSION['imie'] = $imie;
                 $_SESSION['nazwisko'] = $nazwisko;
-                $_SESSION['email'] = $email;
-                $_SESSION['password'] = $password;
                 $_SESSION['id_user'] = $id_user;
-                
-                /* z SESSION mamy:
-                 * zalogowany
-                 * id_user
-                 * imie
-                 * nazwisko
-                 * email
-                 * dialog - tu jest cala rozmowa
-                 */
-            
-                /* z POST mamy:
-                 * l_email
-                 * l_password
-                 */
-            
-                 /* zmienne:
-                  * $sid - tu jest id sesji
-                  */
-            
         }
     }
     if($_POST['wylogowany']){
