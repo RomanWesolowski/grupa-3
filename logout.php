18f4f32b4f09683733d21ce2fd924dc5e856d3d4
Krystian Sobolewski <kleophass@gmail.com>
2016-05-31 14:29:28 +0200

    Naprawione wylogowywanie

--- logout.php
+++ logout.php
@@ -2,13 +2,6 @@
 
 	session_start();
 	include ('connect.php');
-	if ($rezultat = @$polaczenie->query(
-		sprintf("UPDATE `user` SET `FLAGA` = 'NULL' WHERE `user`.`ID_USER` = '".$_SESSION['ID_USER']."'",
-		mysqli_real_escape_string($polaczenie,$login))))
-{
-	session_unset();
-
-	header('Location: index.php');
-}
-
+	if ($rezultat = @$polaczenie->query(		sprintf("UPDATE `user` SET `FLAGA` = 'NULL' WHERE `user`.`ID_USER` = '".$_SESSION['ID_USER']."'")))
+	{		session_unset();		header('Location: index.php');}
 ?>
