bd9668c05ab325045e0a0129362747a597aca482
Krystian Sobolewski <kleophass@gmail.com>
2016-05-31 17:17:05 +0200

    Revert "bledny commit"
    This reverts commit 9a17508350e7b6f9fde4c3256d75dacf5d2d63b2.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="main.css">
     <title>Gadaczek</title>
 </head>
 
