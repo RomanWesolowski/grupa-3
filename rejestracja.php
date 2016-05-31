9a17508350e7b6f9fde4c3256d75dacf5d2d63b2
Krystian Sobolewski <kleophass@gmail.com>
2016-05-31 17:17:00 +0200

    bledny commit

--- rejestracja.php
+++ rejestracja.php
@@ -7,11 +7,11 @@
     {
         //Udana walidacja? Załóżmy, że tak!
         $wszystko_OK=true;
-        
+
         //Sprawdź poprawność  imienia
-        
+
         $nick = $_POST['nick'];
-        
+
         //Sprawdzenie długości imienia
         if ((strlen($nick)<3) || (strlen($nick)>20))
         {
@@ -25,56 +25,56 @@
             $wszystko_OK=false;
             $_SESSION['e_nazwisko']="Nazwisko musi posiadać od 3 do 20 znaków!";
         }
-        
+
         // Sprawdź poprawność adresu email
         $email = $_POST['email'];
         $emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
-        
+
         if ((filter_var($emailB, FILTER_VALIDATE_EMAIL)==false) || ($emailB!=$email))
         {
             $wszystko_OK=false;
             $_SESSION['e_email']="Podaj poprawny adres e-mail!";
         }
-        
+
         //Sprawdź poprawność hasła
         $haslo1 = $_POST['haslo1'];
         $haslo2 = $_POST['haslo2'];
-        
+
         if ((strlen($haslo1)<8) || (strlen($haslo1)>20))
         {
             $wszystko_OK=false;
             $_SESSION['e_haslo']="Hasło musi posiadać od 8 do 20 znaków!";
         }
-        
+
         if ($haslo1!=$haslo2)
         {
             $wszystko_OK=false;
             $_SESSION['e_haslo']="Podane hasła nie są identyczne!";
-        }   
+        }
 
         $haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
-        
+
         //Czy zaakceptowano regulamin?
         if (!isset($_POST['regulamin']))
         {
             $wszystko_OK=false;
             $_SESSION['e_regulamin']="Potwierdź akceptację regulaminu!";
-        }               
-   
-        
+        }
+
+
         //Zapamiętaj wprowadzone dane
-     
+
         $_SESSION['fr_nick'] = $nick;
         $_SESSION['fr_nazwisko'] = $nazwisko;
         $_SESSION['fr_email'] = $email;
         $_SESSION['fr_haslo1'] = $haslo1;
         $_SESSION['fr_haslo2'] = $haslo2;
         if (isset($_POST['regulamin'])) $_SESSION['fr_regulamin'] = true;
-        
+
         include ('connect.php');
         mysqli_report(MYSQLI_REPORT_STRICT);
-        
-        try 
+
+        try
         {
             $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
             if ($polaczenie->connect_errno!=0)
@@ -85,32 +85,32 @@
             {
                 //Czy email już istnieje?
                 $rezultat = $polaczenie->query("SELECT ID_USER FROM USER WHERE EMAIL='$email'");
-                
+
                 if (!$rezultat) throw new Exception($polaczenie->error);
-                
+
                 $ile_takich_maili = $rezultat->num_rows;
                 if($ile_takich_maili>0)
                 {
                     $wszystko_OK=false;
                     $_SESSION['e_email']="Istnieje już konto przypisane do tego adresu e-mail!";
-                }       
+                }
 
                 //Czy nazwisko jest już?
                 $rezultat = $polaczenie->query("SELECT ID_USER FROM USER WHERE NAZWISKO='$nazwisko'");
-                
+
                 if (!$rezultat) throw new Exception($polaczenie->error);
-                
+
                 $ile_takich_nazwisk = $rezultat->num_rows;
                 if($ile_takich_nazwisk>0)
                 {
                     $wszystko_OK=false;
                     $_SESSION['e_nazwisko']="Istnieje już takie nazwisko.";
                 }
-                
+
                 if ($wszystko_OK==true)
                 {
                     //Hurra, wszystkie testy zaliczone, dodajemy do bazy
-                    
+
                     if ($polaczenie->query("INSERT INTO USER VALUES (NULL, '$nick','$nazwisko','$email','$haslo_hash',NULL)"))
                     {
                         $_SESSION['udanarejestracja']=true;
@@ -120,22 +120,22 @@
                     {
                         throw new Exception($polaczenie->error);
                     }
-                    
+
                 }
-                
+
                 $polaczenie->close();
             }
-            
+
         }
         catch(Exception $e)
         {
             echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
             echo '<br />Informacja developerska: '.$e;
         }
-        
+
     }
-    
-    
+
+
 ?>
 
 <!DOCTYPE HTML>
@@ -144,10 +144,10 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Gadaczek rejestracja</title>
 
-    
+
     <style>
         .error
         {
@@ -159,10 +159,10 @@
 </head>
 
 <body>
-    
+
     <section class="center" style="height: 530px;">
         <div class="container">
-        <h1>Rejestracja</h1>    
+        <h1>Rejestracja</h1>
             <form method="post">
                 <div class="form-group">
                     <label for="Imie">Imię</label>
@@ -174,7 +174,7 @@
                         }
                     ?>" name="nick">
                 </div>
-                
+
                 <?php
                     if (isset($_SESSION['e_nick']))
                     {
@@ -200,8 +200,8 @@
                         unset($_SESSION['e_nazwisko']);
                     }
                 ?>
-                
-            
+
+
                 <div class="form-group">
                     <label for="Email">Email</label>
                     <input type="email" class="form-control" id="Email" placeholder="Email" name="email" value="<?php
@@ -211,7 +211,7 @@
                                 unset($_SESSION['fr_email']);
                             }
                         ?>" name="email">
-                </div>   
+                </div>
                 <?php
                     if (isset($_SESSION['e_email']))
                     {
@@ -219,7 +219,7 @@
                         unset($_SESSION['e_email']);
                     }
                 ?>
-                
+
 
                 <div class="form-group">
                     <label for="Password1">Hasło</label>
@@ -237,8 +237,8 @@
                         echo '<div class="error">'.$_SESSION['e_haslo'].'</div>';
                         unset($_SESSION['e_haslo']);
                     }
-                ?>      
-                
+                ?>
+
                 <div class="form-group">
                     <label for="Password2">Powtórz Hasło</label>
                     <input type="password" class="form-control" id="Password2" placeholder="Powtórz Hasło" value="<?php
@@ -248,8 +248,8 @@
                                 unset($_SESSION['fr_haslo2']);
                             }
                         ?>" name="haslo2" >
-                </div>        
-                        
+                </div>
+
 
                 <div class="checkbox">
                 <label>
@@ -268,9 +268,9 @@
                         echo '<div class="error">'.$_SESSION['e_regulamin'].'</div>';
                         unset($_SESSION['e_regulamin']);
                     }
-                ?>  
-                
-                <input type="submit" class="btn btn-primary" value="Zarejestruj się" />        
+                ?>
+
+                <button style="margin-top: 80px" type="submit" class="btn btn-primary">Zarejestruj się</button>
             </form>
         </div>
     </section>
