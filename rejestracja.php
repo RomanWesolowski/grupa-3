c88d099872b697d4b2823c96bcae43bfa32058b3
Michał <zaraski12345@gmail.com>
2016-06-07 17:23:17 +0200

    zmiana ścieżki dostępu do main w rejestracji

--- rejestracja.php
+++ rejestracja.php
@@ -144,7 +144,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Gadaczek rejestracja</title>
 
     
